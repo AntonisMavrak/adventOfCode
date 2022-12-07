1fc0cd2bebff069a2750949c6c0c23fac8dd6f95
Antonis <antonis.mavrak@gmail.com>
2022-12-07 10:29:57 +0200

    Day 3 Part 1

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+echo "<h2>Advent Of Code 2022</h2>";
 echo "<ul>
     <li><a href='/Day1/day1Solution.php'>Day 1</a></li>
     <li><a href='/Day2/day2Solution.php'>Day 2</a></li>
     <li><a href='/Day3/day3Solution.php'>Day 3</a></li>
+    <li><a href='/Day4/day4Solution.php'>Day 4</a></li>
+    <li><a href='/Day5/day5Solution.php'>Day 5</a></li>
 </ul> ";
