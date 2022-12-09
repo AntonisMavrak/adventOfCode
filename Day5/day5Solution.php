fbead5abf145a49222f66d39681133b12c841e02
Antonis <antonis.mavrak@gmail.com>
2022-12-09 10:24:29 +0200

    Day 5 completed

--- Day5/day5Solution.php
+++ Day5/day5Solution.php
@@ -18,14 +18,9 @@ foreach ($helps as $key=>$command){
     $commands[$key]['from'] = $command[0][1];
     $commands[$key]['to'] = $command[0][2];
 }
-// 9 containers
 for($i = 0, $iMax = 8; $i < $iMax; $i++){
     $startingContainers[] = $lines[$i];
 }
-
-
-// --------------------------------------------
-// Part 1
 foreach ($startingContainers as $container){
     $offset = 0;
     for ($i = 1, $iMax = 10; $i < $iMax; $i++){
@@ -40,24 +35,40 @@ foreach ($containers as $key => $container) {
         }
     }
 }
-
+$containersPartTwo = $containers;
+// --------------------------------------------
+// Part 1
 foreach ($commands as $command) {
     for ($i = 1, $iMax = $command['quantity']; $i <= $iMax; $i++) {
         if (count($containers[$command['to']]) === 0) {
             $containers[$command['to']][0] = $containers[$command['from']][key($containers[$command['from']])];
         }
-
         array_unshift($containers[$command['to']], $containers[$command['from']][key($containers[$command['from']])]);
         unset($containers[$command['from']][key($containers[$command['from']])]);
     }
 }
+echo "Top containers in each column: <br><br>";
 foreach ($containers as $container){
-    echo $container[key($container)] . "<br>";
+    echo $container[key($container)];
 }
-
-
+echo "<br> 1  2  3  4  5  6  7  8  9";
+echo "<br>----------------------------<br>";
 // --------------------------------------------
 // Part 2
+foreach ($commands as $key=>$command) {
+    $arrayToBeMoved[] = array_slice($containersPartTwo[$command['from']], 0, $command['quantity'], true);
+    $keysToBeRemoved[] = array_keys($arrayToBeMoved[$key]);
+    foreach (array_reverse($arrayToBeMoved[$key]) as $container){
+        array_unshift($containersPartTwo[$command['to']], $container);
+    }
+    foreach ($keysToBeRemoved[$key] as $keyToBeRemoved){
+        unset($containersPartTwo[$command['from']][$keyToBeRemoved]);
+    }
+}
+foreach ($containersPartTwo as $container){
+    echo $container[key($container)];
+}
+echo "<br> 1  2  3  4  5  6  7  8  9";
 // --------------------------------------------
 // End
 echo "</pre>";
