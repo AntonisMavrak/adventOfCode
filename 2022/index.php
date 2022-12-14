08283a518e370beb74270b0128b3a68d51fc0b9c
Antonis <antonis.mavrak@gmail.com>
2022-12-14 16:05:39 +0200

    init

---
+++ 2022/index.php
@@ -0,0 +1,30 @@
+<?php
+echo "<h2>Advent Of Code 2022</h2>";
+echo "<ul>
+    <li><a href='./Day1/day1Solution.php'>Day 1</a></li>
+    <li><a href='./Day2/day2Solution.php'>Day 2</a></li>
+    <li><a href='./Day3/day3Solution.php'>Day 3</a></li>
+    <li><a href='./Day4/day4Solution.php'>Day 4</a></li>
+    <li><a href='./Day5/day5Solution.php'>Day 5</a></li>
+    <li><a href='./Day6/day6Solution.php'>Day 6</a></li>
+    <li><a href='./Day7/day7Solution.php'>Day 7</a></li>
+    <li><a href='./Day8/day8Solution.php'>Day 8</a></li>
+    <li><a href='./Day9/day9Solution.php'>Day 9</a></li>
+    <li><a href='./Day10/day10Solution.php'>Day 10</a></li>
+    <li><a href='./Day11/day11Solution.php'>Day 11</a></li>
+    <li><a href='./Day12/day12Solution.php'>Day 12</a></li>
+    <li><a href='./Day13/day13Solution.php'>Day 13</a></li>
+    <li><a href='./Day14/day14Solution.php'>Day 14</a></li>
+    <li><a href='./Day15/day15Solution.php'>Day 15</a></li>
+    <li><a href='./Day16/day16Solution.php'>Day 16</a></li>
+    <li><a href='./Day17/day17Solution.php'>Day 17</a></li>
+    <li><a href='./Day18/day18Solution.php'>Day 18</a></li>
+    <li><a href='./Day19/day19Solution.php'>Day 19</a></li>
+    <li><a href='./Day20/day20Solution.php'>Day 20</a></li>
+    <li><a href='./Day21/day21Solution.php'>Day 21</a></li>
+    <li><a href='./Day22/day22Solution.php'>Day 22</a></li>
+    <li><a href='./Day23/day23Solution.php'>Day 23</a></li>
+    <li><a href='./Day24/day24Solution.php'>Day 24</a></li>
+    <li><a href='./Day25/day25Solution.php'>Day 25</a></li>
+</ul> ";
+echo "<a href='../index.php'>Back to main page</a>";
--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
-echo "<h2>Advent Of Code 2022</h2>";
+echo "<h1>Advent Of Code</h1>";
+echo "<h3>Year: </h3>";
 echo "<ul>
-    <li><a href='/2022/Day1/day1Solution.php'>Day 1</a></li>
-    <li><a href='/2022/Day2/day2Solution.php'>Day 2</a></li>
-    <li><a href='/2022/Day3/day3Solution.php'>Day 3</a></li>
-    <li><a href='/2022/Day4/day4Solution.php'>Day 4</a></li>
-    <li><a href='/2022/Day5/day5Solution.php'>Day 5</a></li>
-    <li><a href='/2022/Day6/day6Solution.php'>Day 6</a></li>
-    <li><a href='/2022/Day7/day7Solution.php'>Day 7</a></li>
-    <li><a href='/2022/Day8/day8Solution.php'>Day 8</a></li>
-    <li><a href='/2022/Day9/day9Solution.php'>Day 9</a></li>
-    <li><a href='/2022/Day10/day10Solution.php'>Day 10</a></li>
-    <li><a href='/2022/Day11/day11Solution.php'>Day 11</a></li>
-    <li><a href='/2022/Day12/day12Solution.php'>Day 12</a></li>
-    <li><a href='/2022/Day13/day13Solution.php'>Day 13</a></li>
-    <li><a href='/2022/Day14/day14Solution.php'>Day 14</a></li>
-    <li><a href='/2022/Day15/day15Solution.php'>Day 15</a></li>
-    <li><a href='/2022/Day16/day16Solution.php'>Day 16</a></li>
-    <li><a href='/2022/Day17/day17Solution.php'>Day 17</a></li>
-    <li><a href='/2022/Day18/day18Solution.php'>Day 18</a></li>
-    <li><a href='/2022/Day19/day19Solution.php'>Day 19</a></li>
-    <li><a href='/2022/Day20/day20Solution.php'>Day 20</a></li>
-    <li><a href='/2022/Day21/day21Solution.php'>Day 21</a></li>
-    <li><a href='/2022/Day22/day22Solution.php'>Day 22</a></li>
-    <li><a href='/2022/Day23/day23Solution.php'>Day 23</a></li>
-    <li><a href='/2022/Day24/day24Solution.php'>Day 24</a></li>
-    <li><a href='/2022/Day25/day25Solution.php'>Day 25</a></li>
+    <li><a href='/2022/index.php'>2022</a></li>
+    <li><a href='/2021/index.php'>2021</a></li>
+    <li><a href='/2020/index.php'>2020</a></li>
+    <li><a href='/2022/index.php'>2022</a></li>
 </ul> ";
