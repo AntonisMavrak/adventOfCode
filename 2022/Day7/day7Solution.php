3c5b79dab70aa10544feec28d4aea2514e760e44
Antonis <antonis.mavrak@gmail.com>
2022-12-14 16:05:20 +0200

    Day7

--- 2022/Day7/day7Solution.php
+++ 2022/Day7/day7Solution.php
@@ -1,1 +1,40 @@
 <?php
+echo "<pre>";
+echo "<h2>Day 7</h2>";
+$lines = file("input.txt", FILE_SKIP_EMPTY_LINES);
+$count=0;
+for ($i = 1; $i < 8; $i++){
+    $seperated = explode(" ", trim($lines[$i]));
+
+    if ($seperated[1] !== "ls") {
+        if ($seperated[0] === "dir") {
+            $fileSystem['/'][$seperated[1]] = [];
+        } else {
+            $fileSystem['/']["file".$count] = ['size' => $seperated[0], 'name' => $seperated[1]];
+            $count++;
+        }
+    }
+}
+$root = $fileSystem['/'];
+
+for($i = 8, $iMax = count($lines); $i < $iMax; $i++){
+    $seperated = explode(" ", trim($lines[$i]));
+    if ($seperated[1] === "cd") {
+        if ($seperated[2] === "..") {
+
+        } else {
+            $previousRoot = $root;
+            $root[$seperated[2]] = [];
+            $root = $root[$seperated[2]];
+        }
+    }elseif ($seperated[1] !== "ls") {
+        if ($seperated[0] === "dir") {
+            $root = [];
+        } else {
+            $root["file".$count][] = ['size' => $seperated[0], 'name' => $seperated[1]];
+            $count++;
+        }
+    }
+}
+var_dump($root);
+echo "</pre>";
